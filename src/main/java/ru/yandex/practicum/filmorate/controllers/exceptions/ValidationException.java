537aa2dbf0af2bcb727abfc00498fc3cffa98bb6
DiMagnit <ruder1988@yandex.ru>
2022-07-20 11:58:10 +0300

    fix:main

--- src/main/java/ru/yandex/practicum/filmorate/controllers/exceptions/ValidationException.java
+++
@@ -1,8 +0,0 @@
-package ru.yandex.practicum.filmorate.controllers.exceptions;
-
-public class ValidationException extends RuntimeException {
-
-    public ValidationException(String message) {
-        super(message);
-    }
-}
